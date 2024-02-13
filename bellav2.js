9ba7bfef768e006ffd5c8342f95bd06e610fcba9
Luca <151834727+MapleLeaf93@users.noreply.github.com>
2024-02-13 11:27:53 +0100

    Update bellav2.js

--- bellav2.js
+++ bellav2.js
@@ -1,2 +1,2 @@
 let brusco = "bella chicco dico bella, bella al fratello, bella alla sorella, bella a chi balla a chi si ribella,bella da questa parte anche da quella oh!";
-console.print(brusco);
+console.log(brusco);
